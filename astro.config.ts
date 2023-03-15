fa9c8384374179fe1adfc5575e4b66f6209f5936
romzyyy <fijay0117@gmail.com>
2023-03-15 18:28:20 +0700

    fix compreses

--- astro.config.ts
+++
@@ -1,3 +0,0 @@
-import compress from 'astro-compress'
-
-export default { integrations: [compress()] }
--- webpack.config.js
+++
@@ -1,9 +0,0 @@
-const path = require('path')
-
-module.exports = {
-    mode: 'production',
-    entry: './src/pages/index.astro',
-    output: {
-        path: path.resolve(__dirname, 'output'),
-    },
-}
